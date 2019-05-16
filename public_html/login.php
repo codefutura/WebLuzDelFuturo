3530c8ace0477e474727e721b821cda7a4e3e8c7
michael <michael@michael-PC>
2019-05-16 09:24:36 +0200

    Login

--- public_html/login.php
+++ public_html/login.php
@@ -1,23 +1,33 @@
-<?php 
+<?php session_start();
 
-$usuario = $_POST['usuario'];
-$clave = $_POST['clave'];
+require 'admin/config.php';
+require 'functions.php';
 
-//conectar a la base//
+$errores = '';
 
-$conexion = mysqli_connect("localhost", "root", "", "dLuzDelFuturo");
-$consulta="SELECT * FROM tbl_usuario WHERE usuario_web = '$usuario' and $clave = 'clave'";
-$resultado = mysqli_query($conexion, $consulta);
-$filas = mysqli_num_rows($resultado);
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
+   // $password = hash('sha512', $password);
 
+    $conexion = conexion($bd_config);
+    $statement = $conexion->prepare('SELECT * FROM usuarios WHERE usuario = :usuario AND password = :password AND id_estudiante = :id_estudiante AND usuario_web = 1 :usuario_web');
+    $statement->execute([
+        ':usuario' => $usuario,
+        ':password' => $password,
+        ':id_estudiante' => $id_estudiante,
+        
+    ]);
+    $resultado = $statement->fetch();
 
-if($filas>0){
-    header("Location:estudiante.php");
-} else{
-    echo "Error en la Autentificacion";
+    if($resultado !== false){
+        $_SESSION['usuario'] = $usuario;
+        header('Location:'.RUTA.'validate.php');
+    }else{
+        $errores .= '<li class="error">Tu usuario o contrasena son incorrecto</li>';
+    } 
 }
 
-mysqli_free_result($resultado);
-mysqli_close ($conexion);
+require 'views/login.view.php';
 
 ?>
