6db82b9caecb262a2f7ef734f64747b69c4b8ad0
michael <michael@michael-PC>
2019-05-16 09:53:41 +0200

    Validacion de login en php

--- public_html/login.php
+++ public_html/login.php
@@ -11,19 +11,16 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
    // $password = hash('sha512', $password);
 
     $conexion = conexion($bd_config);
-    $statement = $conexion->prepare('SELECT * FROM usuarios WHERE usuario = :usuario AND password = :password AND id_estudiante = :id_estudiante AND usuario_web = 1 :usuario_web');
+    $statement = $conexion->prepare('SELECT * FROM tbl_usuario WHERE nombre = :usuario AND clave= :password AND usuario_web=1');
     $statement->execute([
         ':usuario' => $usuario,
-        ':password' => $password,
-        ':id_estudiante' => $id_estudiante,
-        ':usuario_web' => $usuario_web,
-        
+        ':password' => $password
     ]);
     $resultado = $statement->fetch();
 
     if($resultado !== false){
         $_SESSION['usuario'] = $usuario;
-        header('Location:'.RUTA.'validate.php');
+        header('Location: notas.php' );
     }else{
         $errores .= '<li class="error">Tu usuario o contrasena son incorrecto</li>';
     } 
