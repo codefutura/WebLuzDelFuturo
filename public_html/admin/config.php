af9788ce65ba15b97116b1f800514afb365b6ac3
michael <michael@michael-PC>
2019-05-16 10:09:50 +0200

    Administrador \ conexion a base de datos

---
+++ public_html/admin/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+define('RUTA', 'http://localhost/PHP/ProyectoLuz/');
+
+//nombre de la base de DATOS
+$bd_config=[
+    'db_name' => 'dbluzdelfuturo',
+    'user' => 'root',
+    'pass' => '1234'
+];
+
+?>
